bb33810e91c39e59b6341f49cf302db3e4bcef78
git push -u origin <mtroyanovsky1@babson.edu>
2017-02-16 17:48:18 -0800

    #18 begin implementation of create new chat endpoint

--- routes/chats.js
+++ routes/chats.js
@@ -6,7 +6,38 @@ var Chat = models.Chat;
 var Message = models.Message;
 var Question = models.Question;
 
-
+router.post('/api/chats/new', function(req, res) {
+  var questionId = req.body.questionId;
+  Question.findById(questionId)
+    .populate('author')
+    .exec(function (err, question) {
+    if(err) {
+      res.status(400).json(err);
+    } else if (!question) {
+      res.status(400).json({message: "question doesn't exist"});
+    } else {
+      //TODO create a new message from the req.body.content
+      var newChat = new Chat({
+        question: questionId,
+        messages: [],
+        questioner: question.author,
+        answerer: req.user,
+        closed: true,
+      })
+      question.chat = newChat._id;
+      req.user.answerChats.push(newChat._id);
+      User.findById(question.author, function (err, user) {
+        if(err) {
+          res.status(400).json(err)
+        } else
+        user.questionChats.push(newChat._id);
+        //Check where user object was created to see whether an empty array was
+        //created. The part you're at right now you just added the chat to
+        //both users
+      })
+    }
+  })
+});
 
 
 
