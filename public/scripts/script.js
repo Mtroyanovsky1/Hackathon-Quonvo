b13499921c6c39c78daa04ee0a0ab0ec1f2acaa9
dnajafi <dbnajafi@gmail.com>
2017-02-16 20:52:59 -0800

    #20 add jquery functionality to questions

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,21 @@
 // YOUR JS CODE FOR ALL PAGES GOES HERE
+$('.questions-list').on('click', '.question', function(event){
+	event.preventDefault();
+	
+	// var question = $(this).children('.question-body-container').text();
+	// console.log($(this).children('.question-body-container').text());
+
+	$.ajax({
+
+		type: "POST",
+		data: {questionId: $(this).children('.question-id').text()},
+		url: '/api/chats/new',
+		success: function(result){
+			console.log(result);
+		}
+	});
+
+});
+
+
+
