cab934f11f066a0023cdc523b4939825065c0c6c
git push -u origin <mtroyanovsky1@babson.edu>
2017-02-17 11:36:39 -0800

    stuff

--- routes/questions.js
+++ routes/questions.js
@@ -6,56 +6,11 @@ var Question = models.Question;
 
 
 router.get('/api/questions', function(req, res){
-	Question.find({state: 'open'}, function(err, questions) {
+	Question.find({}, function(err, questions) {
 		if (err) {
 			res.status(400).json(err);
 		} else {
 			res.json(questions);
-		}
-	});
-});
-
-
-router.get('/api/questions/open', function(req, res) {
-	Question.find({state: 'open'})
-	 .populate('chat')
-	 .exec(function(err, questions) {
-		if (err) {
-			res.status(400).json(err);
-		} else {
-			res.json(questions);
-		}
-	});
-})
-
-
-
-
-router.get('/api/questions/in_progress', function(req, res) {
-	Question.find({state: 'in_progress'})
-	 .populate('chat')
-	 .exec(function(err, questions) {
-		if (err) {
-			res.status(400).json(err);
-		} else {
-			res.json(questions);
-		}
-	});
-})
-
-
-router.get('/api/questions/:questionId/close', function(req, res) {
-	var questionId = req.params.questionId;
-
-	Question.findByIdAndUpdate(questionId, {state: 'closed'}, function(err, question) {
-		if (err) {
-			res.status(400).json(err);
-		} else if (!question) {
-			res.status(404).json({
-				message: "no question with that id"
-			});
-		} else {
-			res.json(question);
 		}
 	});
 });
@@ -88,15 +43,6 @@ router.post('/api/questions/new', function(req, res){
 		if (err) {
 			res.status(400).json(err);
 		} else {
-			// send to all users except author
-			var user_sockets = req.app.settings.user_sockets;
-			for (var userId in user_sockets) {
-				if (user_sockets.hasOwnProperty(userId) && !req.user._id.equals(userId)) {
-					user_sockets[userId].forEach(function(userSocket) {
-						userSocket.emit('appendQuestion', question);
-					});
-				}
-			}
 			res.json(question);
 		}
 	});
